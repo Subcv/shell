3aba031532d555655b44f7b0f25b14624de5ca80
starry <94346255+Subcv@users.noreply.github.com>
2023-01-11 17:30:35 +0800

    Create haose.sh

---
+++ haose.sh
@@ -0,0 +1,47 @@
+#!/bin/bash/
+
+red='\033[0;31m'
+
+green='\033[0;32m'
+
+yellow='\033[0;33m'
+
+plain='\033[0m'
+
+blue='\033[0;34m'
+
+purple='\033[35m'
+
+redwhite='\033[41m'
+
+greenwhite='\033[42m'
+
+yellowwhite='\033[43m'
+
+bluewhite='\033[44m'
+
+purplewhite='\033[45m'
+
+bluewhite='\033[46m'
+
+white="白字"
+
+echo -e ${red}红色${plain}
+
+echo -e ${green}绿色${plain}
+
+echo -e ${yellow}黄色${plain}
+
+echo -e ${purple}紫色${plain}
+
+echo -e ${blue}蓝色${plain}
+
+echo -e ${redwhite}红底$white${plain}
+
+echo -e ${greenwhite}绿底$white${plain}
+
+echo -e ${yellowwhite}黄底$white${plain}
+
+echo -e ${purplewhite}紫底$white${plain}
+
+echo -e ${bluewhite}蓝底$white${plain}
